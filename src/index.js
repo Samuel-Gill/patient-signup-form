62946d7954fa6743a68af327e77d8b2944655bc9
samuelgill1996 <samuelgill1996@gmail.com>
2021-12-07 18:40:22 -0800

    Adding unit-test

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import "antd/dist/antd.css";
+import "./index.less"
 
 ReactDOM.render(
   <>
